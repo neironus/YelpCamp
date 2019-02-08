2e2e6b25228f24704dbf26cc2c8d61c7f3f2feea
maksym_dakal <dakalm@gmail.com>
2019-02-08 23:58:05 +0200

    ended basic setup

--- app.js
+++ app.js
@@ -1,26 +1,37 @@
 const express = require("express")
 const app = express()
-// const bodyParser = require('body-parser')
+const bodyParser = require('body-parser')
 // const request = require("request")
 const port = 3000
 
-// app.use(bodyParser.urlencoded({
-//   extended: true
-// }))
+app.use(bodyParser.urlencoded({ extended: true}))
 app.set("view engine", "ejs")
 app.use(express.static("public"))
+
+var campgrounds = [
+  {name: 'Kitty Terwolbeck', image: "https://farm9.staticflickr.com/8579/16706717975_bdc99767d7.jpg"},
+  {name: 'Ed Hunsinger', image: 'https://farm3.staticflickr.com/2116/2164766085_0229ac3f08.jpg'},
+  {name: 'Kelle Cruz', image: 'https://farm2.staticflickr.com/1424/1430198323_c26451b047.jpg'}
+]
 
 app.get("/", (req, res) => {
   res.render("landing")
 })
 
 app.get("/campgrounds", (req, res) => {
-  const campgrounds = [
-    {name: 'Kitty Terwolbeck', image: "https://farm9.staticflickr.com/8579/16706717975_bdc99767d7.jpg"},
-    {name: 'Ed Hunsinger', image: 'https://farm3.staticflickr.com/2116/2164766085_0229ac3f08.jpg'},
-    {name: 'Kelle Cruz', image: 'https://farm2.staticflickr.com/1424/1430198323_c26451b047.jpg'}
-  ]
   res.render("campgrounds", {campgrounds: campgrounds})
+})
+
+app.post("/campgrounds", (req, res) => {
+  const name = req.body.name
+  const image = req.body.image
+  console.log(name, image)
+  campgrounds.push({name: name, image: image})
+  res.redirect("/campgrounds")
+})
+
+app.get("/campgrounds/new", (req, res) => {
+  res.render("new")
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
